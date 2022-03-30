2d902f3deaab85fe00ab916ff22eb10b18066f2b
sedaakkoyun <sedaakkoyun97@gmail.com>
2022-03-30 15:36:15 +0300

    QuerySelector çalışıldı

--- Dom/tekil-element-secimi.js
+++ Dom/tekil-element-secimi.js
@@ -24,3 +24,19 @@ document.getElementById('header').innerHTML='<b>To do list</b>';
 // Html düzenlendi
 console.log(val);
 
+//QuerySelector
+console.log(document.querySelector('#header'));
+console.log(document.querySelector('.card-header'));
+console.log(document.querySelector('div'));
+document.querySelector('li').style.color='red';
+// İlk li elemanının rengi değişti 1 eleman ıcın ıslem yapar
+document.querySelector('li:last-child').style.color='green';
+//son li rengi değişti
+document.querySelector('li:nth-child(2)').style.color='yellow';
+//2.li elemanının rengi değişti
+document.querySelector('li:last-child').textContent='task item';
+// son li elemanının text içeriği değişti
+// document.querySelector('li').className='list-group-item list-group-item-';
+// Class içeriği değiştirildi
+console.log(document.querySelector('li').classList.add('active'));
+// classların yanına active classıda eklendi

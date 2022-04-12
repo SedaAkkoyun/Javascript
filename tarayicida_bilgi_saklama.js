f09a13f8c73797963ddd4f734b57dbb496e891b9
sedaakkoyun <sedaakkoyun97@gmail.com>
2022-04-12 10:04:40 +0300

    set,get array to storage

--- tarayicida_bilgi_saklama.js
+++ tarayicida_bilgi_saklama.js
@@ -6,21 +6,36 @@ const firstname=localStorage.setItem('firstname','seda');
 // setItem('oluşturulacak parametre ismi','parametre değeri');
 const lastname=localStorage.setItem('lastname','akkoyun');
 
+
+
 //Get ıtem
-const val=localStorage.getItem('firstname');
+// const val=localStorage.getItem('firstname');
 // getItem('çağıralacak parametrenin ismi')
+
+
 
 //Remove ıtem
 
 localStorage.removeItem('firstname');
 
+
+
 //clear
 localStorage.clear();
 // Komple temizler
+
+// set array to storage 
+let hobies=['bisiklet','basketbol','voleybol'];
+localStorage.setItem('hobies',hobies);
+// dizi elemanları değil metin olarak saklar, dizi olarak saklasın istiyorsak;
+localStorage.setItem('hobies',JSON.stringify(hobies));
+
+val=JSON.parse(localStorage.getItem('hobies'));
+// localStorage içerisindeki hobies dizisinin indexli elemanlarını çeker
+
 console.log(val);
-
-
 console.log(localStorage);
+
 
 
 //Session Storage

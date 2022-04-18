0cb6aac82d211137de3ff4844cd61c3007dc3305
sedaakkoyun <sedaakkoyun97@gmail.com>
2022-04-18 14:34:08 +0300

    set-get-delete to localstorage

--- Dom Projesi/Dom-Projesi.js
+++ Dom Projesi/Dom-Projesi.js
@@ -2,7 +2,8 @@ const form=document.querySelector('form');
 const input=document.querySelector('#txtTaskName');
 const btndeleteall=document.querySelector('#btnDeleteAll');
 const tasklist=document.querySelector('#task-list');
-const items=['item 1','item 2','item 3'];
+// const items=['item 1','item 2','item 3'];
+let items;
 
 
 //loadİtem
@@ -21,12 +22,58 @@ function eventListeners(){
 
 
 function loadItems(){
+    items= GetItemFromLs();
+    // Localstorage çağırma fonksiyonu ,
+
+
     items.forEach(function(item){
         createItem(item);
-    })
+    });
     // items dizisinin elemanlarını sıralar ve her bır eleman için createitem fonksiyonunu çalıştırır
 }
 
+// get item to localStorage
+function GetItemFromLs(){
+    if(localStorage.getItem('items')===null){
+        items=[];
+        // Localstorageden çektiğimiz items değeri boş dönüyorsa itemsı []; yap
+    }
+    else{
+        // Eğer boş değilse items değişkenine localstoragenin içindeki değeri yaz
+        items=JSON.parse(localStorage.getItem('items'));
+    }
+    return items;
+}
+
+// Set item to locakstorage
+function SetItemToLS(text){
+
+
+items=GetItemFromLs();
+// İtemsın GetItemFromLs oldugunu söyledik, yani localStorage içerisindeki elemanalr
+items.push(text);
+// functiondan dönen texti itemsa ekledik
+
+localStorage.setItem('items',JSON.stringify(items));
+// localStoragein içerisindeki items elemanına girilen texti ekledik
+// Not: stringify dali ify [] köşeli parantez yapmamızı sağlar
+}
+function DeleteItemFromLs(text){
+    items=GetItemFromLs();
+    // itemın içerisine localStoragedaki 'items' verileri getirildi
+    items.forEach(function(item,index){
+        // veriler sıralandı
+        if(item===text){
+            // gelen texte itemın içerisinden buldu
+          items.splice(index,1);
+        //   buldugu ıtemı sildi splice(buldugun indexin,1. değeri yani ilk değeri)
+        }
+       
+    });
+    localStorage.setItem('items',JSON.stringify(items));
+    // Sildikten sonra son halini localstorageye set etti
+    
+}
 function createItem(text){
 
     const li=document.createElement('li');
@@ -46,9 +93,6 @@ li.appendChild(a);
 tasklist.appendChild(li);
 // li ul içerisine eklendi
 
-
-input.value='';
-// input içi temizlendi
 }
 function addNewItem(e){
 
@@ -58,9 +102,14 @@ if(input.value===''){
 }
 createItem(input.value);
 
+
+// save to ls
+SetItemToLS(input.value);
+// Girilen değer locakstorageye de yazıldı
+
 // console.log(a);
 // console.log(li);
-
+input.value='';
     e.preventDefault();
     //Tıklandığında sayfa yenılenmesin
 }
@@ -71,6 +120,9 @@ if(e.target.className=='fas fa-times')
 
     e.target.parentElement.parentElement.remove();
     // console.log(e.target);
+    
+    DeleteItemFromLs(e.target.parentElement.parentElement.textContent);
+    // fa fa-timesın parenti a, anında parenti li , fakat localstoragede değerler yani texti saklandığı için lininde textcontentini localstorageden sildik.
 }
 
 
@@ -81,19 +133,26 @@ function deleteAll(e){
     // tasklist.innerHTML='';
     // tasklist içerisindeki tüm htmller silindi
     if(confirm('are you sure ?')){
-tasklist.childNodes.forEach(function(item){
+        
+        
+        // Silme 1.yöntem
+// tasklist.childNodes.forEach(function(item){
    
     
-    // Tüm nodesları sıraladık içerisinden nodetype 1 olan yani elementleri(li) seçtik
-    if(item.nodeType===1){
+//     // Tüm nodesları sıraladık içerisinden nodetype 1 olan yani elementleri(li) seçtik
+//     if(item.nodeType===1){
 
-        item.remove();
-        // console.log(item);
+//         item.remove();
+// console.log(item); } });
 
-    }
-})
-    }
 
+        // silme 2.yöntem 
+        while(tasklist.firstChild){
+            // Tasklistin 1.elemanını buldugun sürece
+            tasklist.removeChild(tasklist.firstChild)
+            // tasklistin 1. elemanını sil , her seferinde 1. elemanı sildiği için heps,ni sldiğinde 1. elemanda olmamış olacak
+        }
+localStorage.clear();
     e.preventDefault();
     // Eğer buttonun ıcerısınde a olmasaydı refresh etmezdı sayfayı prevent vermemıze gerek kalmazdı
-}
+} }
